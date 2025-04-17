d12b375813c1d24bbda7b20e94058a8c9e01a38c
Jeff Beck <jbeck8176@gmail.com>
2025-04-17 09:24:04 -0400

    Add lakes.sql script for creating the lakes table

---
+++ database/lakes.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `lakes` (
+  `Id` varchar(36) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `WaterLevelUSGSSiteId` varchar(255) DEFAULT NULL,
+  `WQDataSiteId` int DEFAULT NULL,
+  `Latitude` decimal(10,8) DEFAULT NULL,
+  `Longitude` decimal(11,8) DEFAULT NULL,
+  `CreatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `UpdatedAt` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `WaterTempUSGSSiteId` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
--- lake-data-api/src/Models/LakeWaterLevelModel.cs
+++
@@ -1,7 +0,0 @@
-namespace lake_data_api.Models;
-public class LakeWaterLevel {
-    public string? Id { get; set; }
-    public decimal SurfaceElevation { get; set; }
-    public DateTime WaterLevelTimeStamp { get; set; }
-    public required string LakeId { get; set; }
-}
