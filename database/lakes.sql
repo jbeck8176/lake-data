edd3b7ee0f249a22e902a7b0851fbe16b4c0617c
cforadis <56101229+cforadis@users.noreply.github.com>
2025-04-25 08:08:33 -0400

    Update lakes table definition
    Fixed duplicated field names

--- database/lakes.sql
+++ database/lakes.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `lakes` (
   `Id` varchar(36) NOT NULL,
   `Name` varchar(255) NOT NULL,
-  `WaterLevelUSGSSiteId` varchar(255) DEFAULT NULL,
+  `USGSSiteId` varchar(255) DEFAULT NULL,
   `WQDataSiteId` int DEFAULT NULL,
   `Latitude` decimal(10,8) DEFAULT NULL,
   `Longitude` decimal(11,8) DEFAULT NULL,
